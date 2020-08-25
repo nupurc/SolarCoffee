a5a31b7f02ac052fbc27b8142d957859f665a67d
nupurc <nupurc.chatterjee@gmail.com>
2020-08-25 16:17:43 +0530

    Add Make File and Migrations

---
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SolarCoffee.Data.Model;
+using System;
+ 
+namespace SolarCoffee.Data
+{
+    public class SolarDbContext : IdentityDbContext
+    {
+        public SolarDbContext() { }
+        public SolarDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+        public virtual DbSet<Customer> Cusomers { get; set; }
+        public virtual DbSet<CustomerAddress> CustomerAddress { get; set; }
+    }
+}
