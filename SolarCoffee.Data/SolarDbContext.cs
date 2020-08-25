0fcf6bf0bd3aafe034908ae7440553986ae7f248
nupurc <nupurc.chatterjee@gmail.com>
2020-08-25 17:17:46 +0530

    Add Models

--- SolarCoffee.Data/SolarDbContext.cs
+++ SolarCoffee.Data/SolarDbContext.cs
@@ -12,5 +12,10 @@ namespace SolarCoffee.Data
         public SolarDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
         public virtual DbSet<Customer> Cusomers { get; set; }
         public virtual DbSet<CustomerAddress> CustomerAddress { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductInventory> ProductInventories { get; set; }
+        public virtual DbSet<ProductInventorySnapshot> ProductInventorySnapshots { get; set; }
+        public virtual DbSet<SalesOrder> SalesOrders { get; set; }
+        public virtual DbSet<SalesOrderItem> SalesOrderItems { get; set; }
     }
 }
